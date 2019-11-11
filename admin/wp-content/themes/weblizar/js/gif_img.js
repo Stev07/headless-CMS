a7f0f35dfcee9b4a23d3dbb6a5315940a5b305e7
Steve <steve.dossin@gmail.com>
2019-11-11 18:42:28 +0100

    remove admin

--- admin/wp-content/themes/weblizar/js/gif_img.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function ($) {
-   jQuery( '#accordion-section-themes' ).prepend('<a href="https://weblizar.com"> <img class="wl-upload-img-tag" src="'+gif_image.image_url+'" /></a>');
-});
